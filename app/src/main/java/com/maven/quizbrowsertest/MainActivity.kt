b37299afac0ffad70856e6fb4b573bcfc490a5fd
BillH <hartwg@gmail.com>
2018-01-11 15:00:50 -0500

    Initialized prefs on first run

--- app/src/main/java/com/maven/quizbrowsertest/MainActivity.kt
+++ app/src/main/java/com/maven/quizbrowsertest/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : AppCompatActivity() {
         progressBar.progress = 0
     }
 
+
     @SuppressLint("SetJavaScriptEnabled")
     private fun setupWebView() {
         WebView.setWebContentsDebuggingEnabled(true)
